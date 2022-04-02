217451a9a75c871752a7b4b14465a03dd2a01c75
EvgenDEP1 <jen_dostovalov@mail.ru>
2022-04-02 11:50:43 +0900

    2022

--- step_1.py
+++ step_1.py
@@ -3,7 +3,18 @@ def filter_products(products, seen_prods):
     return target_prods
 
 
+def filter_products_2(products, seen_prods):
+    target_prods = list(set(products) - set(seen_prods))
+    return target_prods
+
+
 print(filter_products(
     [1, 45, 87, 2, 5, 23, 9],
     [87, 1, 5]
 ))
+
+print(filter_products_2(
+    [1, 45, 87, 2, 5, 23, 9],
+    [87, 1, 5]
+))
+
