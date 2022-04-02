9ab7be5c38c849322876de8eb06e4f0e8da18885
EvgenDEP1 <jen_dostovalov@mail.ru>
2022-04-02 11:54:04 +0900

    2022

--- step_1.py
+++ step_1.py
@@ -8,12 +8,22 @@ def filter_products_2(products, seen_prods):
     return target_prods
 
 
+def filter_products_3(products, seen_prods):
+    target_prods = [item for item in products if item not in set(seen_prods)]
+    return target_prods
+
+
 print(filter_products(
     [1, 45, 87, 2, 5, 23, 9],
     [87, 1, 5]
 ))
 
 print(filter_products_2(
+    [1, 45, 87, 2, 5, 23, 9],
+    [87, 1, 5]
+))
+
+print(filter_products_3(
     [1, 45, 87, 2, 5, 23, 9],
     [87, 1, 5]
 ))
